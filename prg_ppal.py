03cdf58264be5e9a23388a34bb4898b6e69e68da
felix.ishema <fehary@yahoo.fr>
2022-11-15 16:58:46 +0100

    creation prg

---
+++ prg_ppal.py
@@ -0,0 +1,22 @@
+'''def fonctionImpair(nombre) :
+    sommeImpair = 0
+    while nombre != 0 :
+        if nombre % 2 != 0:
+# sommeImpair += nombre
+        sommeImpair = sommeImpair + nombre
+        nombre -= 1
+        return sommeImpair'''
+
+
+
+
+
+def fonction_somme (i):
+    for i in list :
+        i = list(i) + i
+    
+
+
+list = [10,20,30,40,50]
+fonction_somme(i)
+
